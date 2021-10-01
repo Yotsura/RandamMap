64c41676745e162d165fcd5be7a74594d3de1952
Yotsura <gxb65358@gmail.com>
2021-10-02 04:36:50 +0900

    あまり行っていない方向へ進むように調整

---
+++ RandomMap/Model/Inclination.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomMapTest.Model
+{
+    /// <summary>
+    /// 1:up 2:right 3:down 4:left
+    /// </summary>
+    public class Inclination
+    {
+        //左上始まりなのではじめは右下行きやすく。
+        int _up { get; set; } = 10;
+        int _right { get; set; } = 0;
+        int _down { get; set; } = 0;
+        int _left { get; set; } = 10;
+
+        public int HotDirection
+            => new List<(int dir, int val)> { (1, _up), (2, _right), (3, _down), (4, _left) }
+                .OrderBy(x => x.val).First().dir;
+        public string Info => $"({_up}/{_right}/{_down}/{_left})";
+
+        public void Up()
+        {
+            _up++;
+        }
+        public void Right()
+        {
+            _right++;
+        }
+        public void Down()
+        {
+            _down++;
+        }
+        public void Left()
+        {
+            _left++;
+        }
+
+    }
+}
--- RandomMap/RundomMap.cs
+++ RandomMap/RundomMap.cs
@@ -30,6 +30,7 @@ namespace RandomMapTest
         int TileCount => MapData.Sum(x => x.Count(y => y == 0));
         readonly Vector3Int _startPos = new Vector3Int { x = 0, z = 0 };
         Vector3Int _goalPos = new Vector3Int { x = 0, z = 0 };
+        Inclination Inclination;
 
         int _seed;
         static Random _random = new Random();
@@ -57,6 +58,7 @@ namespace RandomMapTest
         void Mapping()
         {
             MapData = Enumerable.Range(0, _size).Select(z => Enumerable.Repeat(-1, _size).ToList()).ToList();
+            Inclination = new Inclination();
             var nowPos = _startPos;
             //指定タイルに満たなかった場合はランダムな1:wallから再スタート
             while (TileCount < _minTile)
@@ -65,6 +67,7 @@ namespace RandomMapTest
                 while (MoveNextPos(ref nowPos)) MapData[nowPos.z][nowPos.x] = 0;    //次のposition着色
                 _goalPos = nowPos;
                 nowPos = GetRandamWallPos();
+                IndicateMap();
             }
         }
 
@@ -78,25 +81,47 @@ namespace RandomMapTest
         bool MoveNextPos(ref Vector3Int position)
         {
             //-1:none/0:tile/1:wall
-            var options = new List<Vector3Int>();
+            var options = new List<(int direction, Vector3Int vector)>();
             //4方向のうち範囲内で0:tile以外のマスを追加
-            //right
-            if (position.x + 1 < _size && 0 != MapData[position.z][position.x + 1])
-                options.Add(new Vector3Int { z = position.z, x = position.x + 1 });
-            //left
-            if (0 < position.x && 0 != MapData[position.z][position.x - 1])
-                options.Add(new Vector3Int { z = position.z, x = position.x - 1 });
-            //up
-            if (position.z + 1 < _size && 0 != MapData[position.z + 1][position.x])
-                options.Add(new Vector3Int { z = position.z + 1, x = position.x });
-            //down
+            //up z--
             if (0 < position.z && 0 != MapData[position.z - 1][position.x])
-                options.Add(new Vector3Int { z = position.z - 1, x = position.x });
+                options.Add((1, new Vector3Int { z = position.z - 1, x = position.x }));
+            //right x++
+            if (position.x + 1 < _size && 0 != MapData[position.z][position.x + 1])
+                options.Add((2, new Vector3Int { z = position.z, x = position.x + 1 }));
+            //down z++
+            if (position.z + 1 < _size && 0 != MapData[position.z + 1][position.x])
+                options.Add((3, new Vector3Int { z = position.z + 1, x = position.x }));
+            //left x--
+            if (0 < position.x && 0 != MapData[position.z][position.x - 1])
+                options.Add((4, new Vector3Int { z = position.z, x = position.x - 1 }));
 
+            //四方に壁設置
+            options.ForEach(pos => MapData[pos.vector.z][pos.vector.x] = 1);
             if (options.Count < 1) return false;
-            
-            options.ForEach(pos => MapData[pos.z][pos.x] = 1);
-            position = options[_random.Next(0, options.Count)];
+
+            //inclinationに応じてoption調整
+            var aditional = options.FirstOrDefault(x => x.direction == Inclination.HotDirection);
+            if (aditional.direction > 0)
+                options.Add(aditional);
+
+            var temp = options[_random.Next(0, options.Count)];
+            switch(temp.direction)
+            {
+                case 1:
+                    Inclination.Up();
+                    break;
+                case 2:
+                    Inclination.Right();
+                    break;
+                case 3:
+                    Inclination.Down();
+                    break;
+                case 4:
+                    Inclination.Left();
+                    break;
+            }
+            position = temp.vector;
             return true;
         }
 
@@ -104,6 +129,7 @@ namespace RandomMapTest
         {
             Console.Clear();
             Console.WriteLine($"\r\nsize:{_size},poprate:{_popRate},tile:{TileCount}\r\nseed：{_seed}");
+            Console.WriteLine($"inclination:{Inclination.HotDirection}{Inclination.Info}");
             for (var z = 0; z < _size; z++)
             {
                 var line = "";
